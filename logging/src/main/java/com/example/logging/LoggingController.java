1077e11f5f91510172f4d47d76069769ca7bbecf
LloydAAmaranto <77998199+LloydAAmaranto@users.noreply.github.com>
2024-08-30 14:10:43 -0400

    outputs log messages (INFO,WARN,ERROR,TRACE,DEBUG)

--- logging/src/main/java/com/example/logging/LoggingController.java
+++ logging/src/main/java/com/example/logging/LoggingController.java
@@ -1,13 +1,25 @@
 package com.example.logging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoggingController {
   
-  @RequestMapping("/")
+	Logger logger = LoggerFactory.getLogger(LoggingApplication.class);
+
+	@RequestMapping("/")
   public String index(){
-    return "Welcome to Spring loggging";
+		logger.trace("This is a TRACE message.");
+		logger.debug("This is a DEBUG message.");
+		logger.info("This is a INFO message.");
+		logger.warn("This is a WARN message.");
+		logger.error("This is a ERROR message.");
+    return "Welcome to Spring Logging! Check the console to see the log messages.";
   }
 }
