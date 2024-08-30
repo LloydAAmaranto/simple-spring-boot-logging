0c75fb8feda41cc05b72bfcb0ad99ec7573fbfb1
LloydAAmaranto <77998199+LloydAAmaranto@users.noreply.github.com>
2024-08-30 14:06:13 -0400

    created a working controller

---
+++ logging/src/main/java/com/example/logging/LoggingController.java
@@ -0,0 +1,13 @@
+package com.example.logging;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+  
+  @RequestMapping("/")
+  public String index(){
+    return "Welcome to Spring loggging";
+  }
+}
